daff5a19c318a3daf50fd2b1945a2b1a12653fb1
ManuelSolano15 <2009127@upy.edu.mx>
2021-06-15 22:53:06 -0500

    one

---
+++ Week 6/Chapter4ex02.c
@@ -0,0 +1,17 @@
+/*Chapter4ex02:Write a program to print a block E using astericks (*) where the E
+has a height of seven characters and width of five characters.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("*****\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*****\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*****\n");
+
+    return 0;
+}
