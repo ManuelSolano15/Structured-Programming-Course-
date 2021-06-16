8ad9de543b6eb6a40808b4b38e5959c568fc5786
ManuelSolano15 <2009127@upy.edu.mx>
2021-06-15 23:06:35 -0500

    one

---
+++ Week 6/Chapter4ex04.c
@@ -0,0 +1,17 @@
+/*Chapter4ex04:Write a program to print "HELLO* in big block letters;
+each letter should have a height seven characters and width of five characters.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("H   H  OOOOO  L        A \n");
+    printf("H   H  O   O  L        A  \n");
+    printf("H   H  O   O  L       A A     \n");
+    printf("HHHHH  O   O  L      A   A  \n");
+    printf("H   H  O   O  L      AAAAA \n");
+    printf("H   H  O   O  L      A   A\n");
+    printf("H   H  OOOOO  LLLLL  A   A\n");
+
+    return 0;
+}
