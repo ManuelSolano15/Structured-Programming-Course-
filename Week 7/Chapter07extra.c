407768c609dc6cc5c5c9878a409869ecc532666d
ManuelSolano15 <2009127@upy.edu.mx>
2021-08-12 12:13:02 -0500

    one

---
+++ Week 7/Chapter07extra.c
@@ -0,0 +1,19 @@
+/*Chapter07extra:Program that reads a lowercase character and writes it in
+  capital letters.*/ 
+
+#include <stdio.h> 
+
+int main() 
+{ 
+char answer[1000], lc, cl;
+//lc=lowercase
+//cl= capital letters
+
+printf ("Enter a lowercase character:  "); 
+fgets(answer, sizeof(answer), stdin);
+sscanf(answer, "%c", &lc);
+
+cl = lc - 32; 
+
+printf ("The character %c in capital letters is %c\n", lc, cl); 
+} 
